a3598ab4c343cdff2828fe4f37e6959b13242198
Dmytro Halushko <dima.halushko@ukr.net>
2024-10-22 23:06:43 +0300

    Nats listeners (#1)
    * Refactor NATS helper

--- nats_helper/nats_helper.go
+++ nats_helper/nats_helper.go
@@ -8,57 +8,72 @@ import (
 	"time"
 )
 
-type NatsMessageHandler func(*nats.Msg)
+type NatsMessage struct {
+	UserId int64  `json:"userId"`
+	Text   string `json:"natsMessage"`
+}
 
-type NatsListener struct {
-	Handler NatsMessageHandler
+type Command struct {
+	UserId    int64    `json:"userId"`
+	Arguments []string `json:"arguments"`
+}
+
+type NatsListenerHandlerFunction func(*nats.Msg)
+
+type NatsListenerHandler struct {
+	Function NatsListenerHandlerFunction
 }
 
 //goland:noinspection GoUnusedExportedFunction
-func PublishToNATS(natsQueue string, message []byte) error {
+func PublishToNATS(queue string, message []byte) error {
 	nc, err := connect()
 	if err != nil {
+		log.Printf("[PublishToNATS] Помилка під'єднання до NATS (черга \"%s\"): %v", err, queue)
 		return err
 	}
 	defer nc.Close()
 
-	err = nc.Publish(natsQueue, message)
-	if err != nil {
+	if err = nc.Publish(queue, message); err != nil {
+		log.Printf("[PublishToNATS] Помилка публікації в чергу \"%s\" NATS: %v", queue, err)
 		return err
 	}
 
-	log.Printf("[PublishToNATS] Повідомлення надіслано в NATS")
+	log.Printf("[PublishToNATS] Повідомлення надіслано в чергу \"%s\" NATS", queue)
 	return nil
 }
 
 //goland:noinspection GoUnusedExportedFunction
-func StartNatsListener(queueName string, function *NatsListener) *nats.Conn {
+func StartNatsListener(queue string, handler *NatsListenerHandler) error {
 	nc, err := connect()
 	if err != nil {
-		log.Printf("[NatsListener] Error connecting to NATS: %v", err)
+		log.Printf("[StartNatsListener] Помилка під'єднання до NATS (черга \"%s\"): %v", err, queue)
 		return nil
 	}
-	if _, err = nc.Subscribe(queueName, func(msg *nats.Msg) { function.Handler(msg) }); err != nil {
-		log.Printf("[NatsListener] Error subscribing to NATS queue: %v", err)
-		return nil
-	}
-	err = nc.Flush()
-	if err != nil {
-		log.Printf("[NatsListener] Error during NATS flush: %v", err)
-		return nil
-	}
-	if err = nc.LastError(); err != nil {
-		log.Printf("[NatsListener] NATS subscription error: %v", err)
+	if _, err = nc.Subscribe(queue, func(msg *nats.Msg) { handler.Function(msg) }); err != nil {
+		log.Printf("[StartNatsListener] Помилка підписки до черги \"%s\" в NATS: %v", queue, err)
+		return err
 	}
 
-	log.Println("[NatsListener] NATS subscription successful")
-	return nc
+	if err = nc.Flush(); err != nil {
+		log.Printf("[StartNatsListener] Помилка flash в черзі \"%s\" NATS : %v", queue, err)
+		return err
+	}
+
+	if err = nc.LastError(); err != nil {
+		log.Printf("[StartNatsListener] Помилка для черги \"%s\" в NATS: %v", queue, err)
+		return err
+	}
+
+	log.Printf("[StartNatsListener] Підписка до черги \"%s\" вдала", queue)
+	return nil
 }
 
 func connect() (*nats.Conn, error) {
 	ip := os.Getenv("BROKER_IP")
 	port := os.Getenv("BROKER_PORT")
 	natsUrl := fmt.Sprintf("nats://%s:%s", ip, port)
+
+	log.Printf("[connect] Під'єднуюся до NATS: %s", natsUrl)
 
 	for i := 0; i < 5; i++ {
 		nc, err := nats.Connect(natsUrl)
@@ -67,7 +82,9 @@ func connect() (*nats.Conn, error) {
 			time.Sleep(3 * time.Second)
 			continue
 		}
+
+		log.Printf("[connect] Під'єднався до NATS: %s", natsUrl)
 		return nc, nil
 	}
-	return nil, fmt.Errorf("[NatsListener] Unable to connect to NATS")
+	return nil, fmt.Errorf("[connect] Неможливо під'єднатися до NATS: %s", natsUrl)
 }
